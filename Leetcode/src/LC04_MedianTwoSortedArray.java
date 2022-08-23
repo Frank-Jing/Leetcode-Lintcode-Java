351db1f36682b42871e8182da3c124e81bffc7e9
Frank Jing <jingwx36@gmail.com>
2022-08-23 08:49:10 -0400

    Update LC04_MedianTwoSortedArray.java

--- Leetcode/src/LC04_MedianTwoSortedArray.java
+++ Leetcode/src/LC04_MedianTwoSortedArray.java
@@ -1,63 +1,62 @@
 package Leetcode.src;
 
+import java.sql.Time;
+
 public class LC04_MedianTwoSortedArray {
     class Solution {
-        //binary search
-        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-            int n1 = nums1.length;
-            int n2 = nums2.length;
-            if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        /*binary search
+        * Now try to find the i where a[i-1] <= b[j] and b[j-1] <= a[i]. So this i is partition around which lies the median.
+        *
+        * Time complexity is O(log(min(x, y))
+        * Space complexity is O(1)
+        * https://leetcode.com/problems/median-of-two-sorted-arrays/
+        * https://discuss.leetcode.com/topic/4996/share-my-o-log-min-m-n-solution-with-explanation/4
+        */
 
-            int k = (n1 + n2 + 1)/2;
+        public static double findMedianSortedArrays(int[] input1, int[] input2) {
+            //if input1 length is greater than switch them so that input1 is smaller than input2.
+            if (input1.length > input2.length) {
+                return findMedianSortedArrays(input2, input1);
+            }
+            int x = input1.length;
+            int y = input2.length;
 
-//         if(n1 == 0){
-//             if(n2%2 == 1) return (double) nums2[k-1];
-//             else return (double)(nums2[k-1] + nums2[k])*0.5;
-//         }
+            int low = 0;
+            int high = x;
+            while (low <= high) {
+                int partitionX = (low + high)/2;
+                int partitionY = (x + y + 1)/2 - partitionX;
 
-//         if(n1 == 1 && n2 == 1) return (double)(nums1[0] + nums2[0])*0.5;
+                //if partitionX is 0 it means nothing is there on left side. Use -INF for maxLeftX
+                //if partitionX is length of input then there is nothing on right side. Use +INF for minRightX
+                int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : input1[partitionX - 1];
+                int minRightX = (partitionX == x) ? Integer.MAX_VALUE : input1[partitionX];
 
-//         if(nums1[n1-1] < nums2[0]){
-//             if(n1 == n2) return (double)(nums1[n1-1] + nums2[0])*0.5;
-//             if((n1+n2)%2 == 1){
-//                 return (double)nums2[k-n1-1];
-//             }else{
-//                 return (double)((nums2[k-n1-1] + nums2[k-n1])*0.5);
-//             }
-//         }
+                int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : input2[partitionY - 1];
+                int minRightY = (partitionY == y) ? Integer.MAX_VALUE : input2[partitionY];
 
-//         if(nums1[0] > nums2[n2-1]){
-//             if(n1 == n2) return (double)(nums2[n2-1] + nums1[0])*0.5;
-//             if((n1+n2)%2 == 1){
-//                 return (double)nums2[n2 - (k-n1)];
-//             }else{
-//                 return (double)((nums2[n2 - (k-n1) - 1] + nums2[n2 - (k-n1)])*0.5);
-//             }
-//         }
-
-
-            int left=0, right=n1;
-            while(left < right){
-                int mid = (left+right)/2;
-                if(nums1[mid] < nums2[k-mid-1]){
-                    left = mid + 1;
-                }else{
-                    right = mid;
+                if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                    //We have partitioned array at correct place
+                    // Now get max of left elements and min of right elements to get the median in case of even length combined array size
+                    // or get max of left for odd length combined array size.
+                    if ((x + y) % 2 == 0) {
+                        return ((double)Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY))/2;
+                    } else {
+                        return (double)Math.max(maxLeftX, maxLeftY);
+                    }
+                } else if (maxLeftX > minRightY) { //we are too far on right side for partitionX. Go on left side.
+                    high = partitionX - 1;
+                } else { //we are too far on left side for partitionX. Go on right side.
+                    low = partitionX + 1;
                 }
             }
 
-            double res = (double)Math.max(left == 0? Integer.MIN_VALUE: nums1[left-1],
-                    k-left ==0? Integer.MIN_VALUE: nums2[k-left-1]);
-            if((n1+n2)%2 == 0){
-                int next = Math.min(left == n1? Integer.MAX_VALUE: nums1[left],
-                        k-left == n2? Integer.MAX_VALUE: nums2[k-left]);
-                res = (double) (res+next)/2;
-            }
-            return res;
+            //Only we can come here is if input arrays were not sorted. Throw in that scenario.
+            throw new IllegalArgumentException();
         }
 
         //merge sort but not fastest
-        public double mergedArrayMedian(int[] nums1, int[] nums2){
+        public static double mergedArrayMedian(int[] nums1, int[] nums2){
             int[] mergedList = new int[nums1.length + nums2.length];
             int i=0,j=0,k=0;
             while(i<nums1.length && j<nums2.length){
@@ -77,6 +76,12 @@ public class LC04_MedianTwoSortedArray {
                 return + mergedList[k/2];
             }
 
+        }
+
+        public static void main(String[] args) {
+            int[] x = {1, 3, 8, 9, 15};
+            int[] y = {7, 11, 19, 21, 18, 25};
+            findMedianSortedArrays(x, y);
         }
     }
 }
