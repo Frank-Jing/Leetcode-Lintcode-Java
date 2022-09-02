11c6d220300e48be4c57d5924546a2e51a0fc65c
Frank-Jing <jingwx36@gmail.com>
2022-09-02 08:23:46 -0400

    solve4Problems

---
+++ Leetcode/src/LC141_LinkedListCycle.java
@@ -0,0 +1,18 @@
+package Leetcode.src;
+
+public class LC141_LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) return false;
+
+        ListNode fast = head.next.next, slow = head.next;
+        while(fast != slow){
+            if(fast == null || fast.next == null){
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return true;
+    }
+}
